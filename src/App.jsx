be76ae155f0877ccd0603f78d0595291e19b38b5
Trilokinath Chaubey <chaubeytn2103@outlook.com>
2024-10-09 22:19:38 +0530

    used axios for getting the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import axios from 'axios';
 const App = () => {
+
+  const getproducts = () => {
+    const api = 'https://fakestoreapi.com/products';
+    axios.get(api).then(products => {
+      console.log(products);
+    }).catch(error => {
+      console.log(error);
+    })
+
+  }
+
+
   return (
-    <div>App</div>
+    <div className="pt-[5%] pl-[5%] mt-4">
+      <button onClick={getproducts} className="px-5 py-2 bg-red-300 rounded-lg">Call APi products</button>
+    </div >
   )
 }
 
-export default App
+export default App;
